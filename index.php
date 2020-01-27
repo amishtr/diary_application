4616d92d0d6adc054c1aa736e025de70408ae432
gbtrivea <amish.trivedi@sony.com>
2020-01-27 12:00:12 +0000

    version 2.0.0

--- index.php
+++ index.php
@@ -39,7 +39,7 @@ $userName = $row["userName"];
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?php echo $_SESSION['userName']. " | Homepage"; ?></title>
+    <title><?php echo $userName. " | Homepage"; ?></title>
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" type="text/css" href="css/line-awesome.min.css">
